f205f6fbe92565e97e2aaa2061547812df4b358e
Boruev Isroiljon <140053898+b-isroiljon@users.noreply.github.com>
2024-03-19 06:20:30 +0600

    docs: Extracting TableHeader.jsx

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -43,14 +43,7 @@ class Movies extends Component {
     this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
   };
 
-  handleSort = (path) => {
-    const sortColumn = { ...this.state.sortColumn };
-    if (sortColumn.path === path)
-      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-    else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
+  handleSort = (sortColumn) => {
     this.setState({ sortColumn });
   };
 
@@ -88,7 +81,7 @@ class Movies extends Component {
           <p>Showing {filtered.length} movies in the database</p>
           <MoviesTable
             movies={movies}
-            // sortColumn={sortColumn}
+            sortColumn={sortColumn}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
             onSort={this.handleSort}
