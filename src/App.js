f5dcdb453a411b1ff814873e7b30219b3c859287
Boruev Isroiljon <140053898+b-isroiljon@users.noreply.github.com>
2024-03-20 21:50:57 +0600

    extracting a reusable input

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Customers from "./customers";
 import Movies from "./components/movies";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import MoviesForm from "./components/moviesForm";
 import NavBar from "./components/navBar";
 import "./App.css";
 
@@ -15,6 +16,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/movies" component={Movies} />
+            <Route path="/movies" component={MoviesForm} />
             <Route path="/customers" component={Customers} />
             <Route path="/rentals" component={Rentals} />
             <Route path="/not-found" component={NotFound} />
---
+++ src/components/loginForm.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+
+class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmite}>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <div className="from-group">
+            <label htmlFor="password">Username</label>
+            <input id="password" type="text" className="form-control" />
+          </div>
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
